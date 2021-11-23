41e6ad75a65a6acba1f3dda04680fe9454023f01
wSwanepoel199 <wjaco.swanepoel@gmail.com>
2021-11-23 14:45:48 +1100

    fixed some bugs and unintended features related to navbar and images on blog

--- scr/scripts/main.js
+++ scr/scripts/main.js
@@ -1,18 +1,34 @@
+
 // Used for burger button animation and opening mobile navbar
 const menuBtn = document.querySelector('.menu-btn');
 const navResp = document.querySelector('.topnav');
 const secResp = document.querySelector('#navbar');
+const node = document.querySelector('.topnav');
+const findClasses = (navResp, all, ...classes) => {
+  return all ?
+    classes.every(cls => node.classList.contains(cls)) :
+    classes.some(cls => node.classList.contains(cls));
+};
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
+  if(!findClasses(navResp, false, 'fixed') && !menuOpen) {
     menuBtn.classList.add('open');
     navResp.classList.add('responsive');
     secResp.classList.add('response');
     menuOpen = true;
-  } else {
+  } 
+  else if (findClasses(navResp, true, 'fixed') && !menuOpen){
+    menuBtn.classList.add('open');
+    navResp.classList.add('fixedResponse');
+    navResp.classList.add('responsive');
+    secResp.classList.add('response');
+    menuOpen = true;
+  }
+  else {
     menuBtn.classList.remove('open');
     navResp.classList.remove('responsive');
     secResp.classList.remove('response');
+    navResp.classList.remove('fixedResponse');
     menuOpen = false;
   }
 });
@@ -20,13 +36,16 @@ menuBtn.addEventListener('click', () => {
 // When scrolling past Navbar this will cause the bar to become fixed to top of screen for easy constant access
 var height = $('.header').height();
 $(window).scroll(function() {
-  if($(this).scrollTop() > height){
-    $('.topnav').addClass('fixed');
+  if($(this).scrollTop() > height && !menuOpen) {
+    navResp.classList.add('fixed');
+  }else if($(this).scrollTop() > height && menuOpen){
+    navResp.classList.add('fixed');
+    navResp.classList.replace('responsive', 'fixedResponse');
   }else{
-    $('.topnav').removeClass('fixed');
+    navResp.classList.remove('fixed');
+    navResp.classList.replace('fixedResponse', 'responsive');
   }
 });
-
 // Used for porting main, most recent, blog post from the blog page to the main index page, also updates image path and replaces img parent class "fakeimg" with "realimg".
 var id = $("#active");
 if (id.text().indexOf("Home") !== -1){
